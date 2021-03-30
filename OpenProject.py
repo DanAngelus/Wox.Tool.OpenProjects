ed23544e9e6450d1cb6af62369e1dea84d8d9676
dbennett_tier2 <dbennett@tier2consulting.com>
2021-03-30 15:02:57 +0100

    [major] Initial commit

---
+++ OpenProject.py
@@ -0,0 +1,12 @@
+from wox import Wox
+from subprocess import Popen
+
+class OpenProject(wox):
+
+    def query(self, userInput):
+        # List projects by finding .iml files and displaying name by / / / under Projects directory
+        # Open using idea {path}
+        pass
+
+if __name__ == '__main__':
+    OpenProject()
